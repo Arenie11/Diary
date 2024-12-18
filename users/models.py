c312116cbac772ff5824c70d0d3ad984afe6616a
Zainab Alugo <zainabalugo@gmail.com>
2024-12-18 14:30:38 +0100

    models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+GENDER=(
+        ('M', 'Male'),
+        ('F', 'Female')
+
+)
+
+class Profile(models.Model):
+    user= models. OneToOneField(User,on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    profile_pix= models.ImageField(upload_to='profile', blank=True, null=True )
+    def __str__(self):
+        return self.fullname
+
+    
