1f06ae9b35049b7be6614df59721e490321f2911
Zainab Alugo <zainabalugo@gmail.com>
2024-12-18 14:35:03 +0100

    user urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-urlpatterns=[
+from . import views
 
-
-
+urlpatterns= [
+    path('register/', views.RegistrationView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
+    path('dashboard/', views. DashboardView.as_view(),name='dashboard'),
+    path('update/', views. DashboardView.as_view(),name='update'),
+    
 ]
